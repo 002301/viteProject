e88d004e77478e5a864451a17c072153f7a818db
马宁 <ma_ning@corp.netease.com>
2023-06-16 18:02:34 +0800

    提出bgm.vue功能

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="app">
-      <!-- <div class="music" :class="{active:playing}" @click="onMusic">
-        <audio :src="bgm" loop id="bgm" ></audio>
-      </div> -->
+    <Bgm ></Bgm>
     <transition-group name="fade">
       <Loading v-if="pageIndex === -1" :assets="loadAssets" @loaded="loadingLoaded" ></Loading>
     </transition-group>
@@ -17,18 +15,7 @@ const loadAssets = import.meta.globEager('./assets/**/*.(png|jpg)');
 const loadingLoaded = ()=>{
   pageIndex = 0
 }
-let playing = $ref(false)
-const onMusic = ()=>{
-  if(playing){
-    playing = false;
-    document.querySelector('#bgm').pause()
-    
-  }else{
-    playing = true;
-    document.querySelector('#bgm').play()
-  }
-  
-}
+
 //初始化页数
 let pageIndex = $ref(-1);
 const setPage = (params) => {
@@ -41,23 +28,5 @@ provide('setPage',setPage)
 <style lang="scss" scoped>
 .app{
   width: 100%;height: 100%;
-  .music{
-      position: absolute;
-      z-index: 1000;
-      top:20rpx;right:50rpx;
-      background: url(./assets/music.png#y2);
-  }
-  .music.active{
-    background-position: 0 100%;
-    animation:r 4s linear infinite;
-  }
-}
-@keyframes r{
-  0%{
-    transform: rotate(0);
-  }
-  100%{
-    transform: rotate(360deg);
-  }
 }
 </style>
---
+++ src/components/Bgm.vue
@@ -0,0 +1,48 @@
+<template>
+  <teleport to="body">
+      <div class="bgm" :class="{active:playing}" @click="onMusic">
+        <audio :src="src" loop id="bgm" ></audio>
+      </div>
+  </teleport>
+</template>
+
+<script setup>
+import { onMounted, ref , reactive } from 'vue';
+//传入音频地址
+const {src} = defineProps(['src'])
+
+let playing = $ref(false)
+const onMusic = ()=>{
+  if(playing){
+    playing = false;
+    document.querySelector('#bgm').pause()
+    
+  }else{
+    playing = true;
+    document.querySelector('#bgm').play()
+  }
+  
+}
+
+</script>
+
+<style lang="scss" scoped>
+.bgm{
+    position: absolute;
+    z-index: 1000;
+    top:20rpx;right:50rpx;
+    background: url(../assets/music.png#y2);
+}
+.music.active{
+  background-position: 0 100%;
+  animation:r 4s linear infinite;
+}
+@keyframes r{
+  0%{
+    transform: rotate(0);
+  }
+  100%{
+    transform: rotate(360deg);
+  }
+}
+</style>
